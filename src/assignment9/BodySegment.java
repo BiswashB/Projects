f6df8cd65c70bd4b0bdc5552ed675236ffe0ddc9
biswa <biswa@Biswash.dhcp.wustl.edu>
2023-12-08 13:06:13 -0600

    gfh

--- src/assignment9/BodySegment.java
+++ src/assignment9/BodySegment.java
@@ -8,18 +8,48 @@ public class BodySegment {
 
 	private double x, y, size;
 	private Color color;
-	
+
 	public BodySegment(double x, double y, double size) {
-		//FIXME
-		
-		//See ColorUtils for some color options (or choose your own)
+//FIXME
+		color = ColorUtils.solidColor();
+		this.x = x;
+		this.y = y;
+		this.size = size;
+
+//See ColorUtils for some color options (or choose your own)
 	}
-	
+
 	/**
 	 * Draws the segment
 	 */
 	public void draw() {
-		//FIXME
+//FIXME
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.filledCircle(x, y, size);
 	}
-	
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getSize() {
+// TODO Auto-generated method stub
+		return this.size;
+	}
+
+	public void setSize(double size) {
+		this.size = size;
+	}
 }
--- src/assignment9/Game.java
+++ src/assignment9/Game.java
@@ -5,30 +5,40 @@ import java.awt.event.KeyEvent;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Game {
-	
+	Snake newSnake;
+	Food newFood;
+
 	public Game() {
 		StdDraw.enableDoubleBuffering();
-		
-		//FIXME - construct new Snake and Food objects
+//FIXME - construct new Snake and Food objects
+		newSnake = new Snake();
+		newFood = new Food();
 	}
-	
+
 	public void play() {
-		while (true) { //TODO: Update this condition to check if snake is in bounds
+		while (newSnake.isInbounds()) { // TODO: Update this condition to check if snake is in bounds
 			int dir = getKeypress();
-			//Testing only: you will eventually need to do more work here
+//Testing only: you will eventually need to do more work here
 			System.out.println("Keypress: " + dir);
-			
+			newSnake.changeDirection(dir);
+			newSnake.move();
+			updateDrawing();
+			if (newSnake.eatFood(newFood) == true) {
+				newFood = new Food();
+				updateDrawing();
+			}
+			System.out.println("Keypress: End of Loop");
+
 			/*
-			 * 1. Pass direction to your snake
-			 * 2. Tell the snake to move
-			 * 3. If the food has been eaten, make a new one
-			 * 4. Update the drawing
+			 * 1. Pass direction to your snake 2. Tell the snake to move 3. If the food has
+			 * been eaten, make a new one 4. Update the drawing
 			 */
 		}
+		drawGameOverScreen();
 	}
-	
+
 	private int getKeypress() {
-		if(StdDraw.isKeyPressed(KeyEvent.VK_W)) {
+		if (StdDraw.isKeyPressed(KeyEvent.VK_W)) {
 			return 1;
 		} else if (StdDraw.isKeyPressed(KeyEvent.VK_S)) {
 			return 2;
@@ -40,22 +50,57 @@ public class Game {
 			return -1;
 		}
 	}
-	
+
 	/**
 	 * Clears the screen, draws the snake and food, pauses, and shows the content
 	 */
 	private void updateDrawing() {
-		//FIXME
-		
+
+		StdDraw.clear();
+		newSnake.draw();
+		newFood.draw();
+		StdDraw.pause(50);
+		StdDraw.show();
+
 		/*
-		 * 1. Clear screen
-		 * 2. Draw snake and food
-		 * 3. Pause (50 ms is good)
-		 * 4. Show
+		 * 1. Clear screen 2. Draw snake and food 3. Pause (50 ms is good) 4. Show
 		 */
 	}
-	
+
+	public static void drawStart() {
+//StdDraw.clear(StdDraw.BOOK_BLUE);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.text(0.5, 0.5, "WELCOME TO SNAKE!!!!!!!");
+		StdDraw.pause(2000);
+		StdDraw.clear();
+	} 
+
+	private void drawGameOverScreen() {
+		StdDraw.clear();
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.text(0.5, 0.6, "GAME OVER");
+		StdDraw.text(0.5, 0.55, "Press 'R' to restart or 'Q' to quit");
+
+		StdDraw.show();
+
+		waitForInput();
+	}
+
+	private void waitForInput() {
+		while (true) {
+			if (StdDraw.isKeyPressed(KeyEvent.VK_R)) {
+				newSnake = new Snake();
+				newFood = new Food();
+				play();
+				break;
+			} else if (StdDraw.isKeyPressed(KeyEvent.VK_Q)) {
+				System.exit(0);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
+		drawStart();
 		Game g = new Game();
 		g.play();
 	}
